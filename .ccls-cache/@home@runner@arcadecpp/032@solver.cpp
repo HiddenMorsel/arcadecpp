76898439242045aaa8ca623dc8dc7b48f9618993
Thales Félix <54778378+HiddenMorsel@users.noreply.github.com>
2023-04-13 00:51:15 +0000

    Atualização de algumas questões até a aula do dia 13.

--- .ccls-cache/@home@runner@arcadecpp/032@solver.cpp
+++ .ccls-cache/@home@runner@arcadecpp/032@solver.cpp
@@ -6,37 +6,39 @@
 using namespace fn;
 
 std::vector<int> get_men(std::vector<int> vet) {
-    return {}; // todo
+	//To do
 }
 
 std::vector<int> get_calm_women(std::vector<int> vet) {
-    return {}; // todo
+	//To do
 }
 
 std::vector<int> sort(std::vector<int> vet) {
-    return {}; // todo
+	//To do
 }
 
 std::vector<int> sort_stress(std::vector<int> vet) {
-    return {}; // todo
+	//To do
 }
 
 std::vector<int> reverse(std::vector<int> vet) {
-    return {}; // todo
+	//To do
+}
+
+void reverse_inplace(std::vector<int> &vet) {
+	//To do
 }
 
 std::vector<int> unique(std::vector<int> vet) {
-    return {}; // todo
+	//To do
 }
 
 std::vector<int> repeated(std::vector<int> vet) {
-    return {}; // todo
-
+	//To do
 }
 
-
 int main() {
-    auto strToVet = [](auto s) { return s.substr(1, s.size() - 2) | SPLIT(',') | MAP(FNT(x, (int)+x)); };
+    auto strToVet = [](auto s) { return map(split(s.substr(1, s.size() - 2), ','), FNT(x, (int)+x)); };
 
     while(true) {
         std::string line = input();
@@ -48,10 +50,13 @@ int main() {
         else if(args[0] == "sort"           ) { write(sort(strToVet(args[1])));           }
         else if(args[0] == "sort_stress"    ) { write(sort_stress(strToVet(args[1])));    }
         else if(args[0] == "reverse"        ) { write(reverse(strToVet(args[1])));        }
+        else if(args[0] == "reverse_inplace") { 
+            auto vet = strToVet(args[1]);
+            reverse_inplace(vet);
+            write(vet);
+        }
         else if(args[0] == "unique"         ) { write(unique(strToVet(args[1])));         }
         else if(args[0] == "repeated"       ) { write(repeated(strToVet(args[1])));       }
         else if(args[0] == "end"            ) { break;                                    }
     }
 }
-
-
--- .ccls-cache/@home@runner@arcadecpp/032@stress3_o_inimigo_agora_e_outro.cpp
+++ .ccls-cache/@home@runner@arcadecpp/032@stress3_o_inimigo_agora_e_outro.cpp
@@ -1,4 +1,8 @@
-#include<iostream>
+/*
+Tempo : 180 min.
+-Eu sou meio lerdo e ruim de lógica então algumas funções demoraram muito mais tempo que outras.
+-Com exceção da ajuda pedida ao grupo da monitoria e o geral de POO sobre a main e a função reverse, o código foi feito sozinho, MAS eu pesquisei algoritmo e exemplos de código para problemas cuja lógica tive problemas de imaginar sozinho. Principalmente a ajuda do Geeks for Geeks.
+*/#include<iostream>
 #include<fn.hpp>
 
 bool is_man(int value) {
@@ -15,21 +19,121 @@ std::vector<int> get_men(std::vector<int> vet) {
   return vetMen;
 }
 
+std::vector<int> get_calm_women(std::vector<int> vet) {
+  std::vector<int>vetCalmWomen;
+    for(long unsigned int i = 0; i < vet.size(); i++){
+      if( vet[i] < 0 && vet[i] > -10) vetCalmWomen.push_back(vet[i]);
+    }
+  return vetCalmWomen;
+}
 
+std::vector<int> sort(std::vector<int> vet) {
+	int aux{0};
+
+  for(long unsigned int i = 0; i < vet.size(); i++){
+      for(long unsigned int j = i +1; j < vet.size(); j++){
+        if(vet[i] > vet[j]){
+          aux = vet[i];
+          vet[i] = vet[j];
+          vet[j] = aux;
+        }
+      }
+    }
+  return vet;
+}
+
+std::vector<int> sort_stress(std::vector<int> vet) {
+	int aux{0};
+
+  for(long unsigned int i = 0; i < vet.size(); i++){
+      for(long unsigned int j = i +1; j < vet.size(); j++){
+        if(abs(vet[i]) > abs(vet[j])){
+          aux = vet[i];
+          vet[i] = vet[j];
+          vet[j] = aux;
+        }
+      }
+    }
+  return vet;
+}
+
+std::vector<int> reverse(std::vector<int> vet) {
+	std::vector<int> vetInvertido(0);
+  if(vet.size() == 0) return vetInvertido;
+  
+  int vetSize = vet.size();
+  int end{vetSize - 1};
+  
+  while(vetInvertido.size() < vet.size()) {
+    vetInvertido.push_back(vet[end]);
+    end--;
+  }
+  return vetInvertido;
+}
+
+bool is_unico(std::vector<int> vet, int value){
+  for(auto i = 0; i < vet.size(); i++){
+    if( value == vet[i]) return true;
+  }
+  return false;
+}
+
+std::vector<int> unique(std::vector<int> vet) {
+  
+  if(vet.size() == 0) return vet;
+
+  std::vector<int> vetUnico;
+  vetUnico.push_back(vet[0]);
+  
+	for(auto i = 0; i < vet.size(); i++){
+      if(!is_unico(vetUnico, vet[i]) ) {
+        vetUnico.push_back(vet[i]);
+      }
+    }
+  return vetUnico;
+}
+
+std::vector<int> repeated(std::vector<int> vet) {
+  
+  if(vet.size() == 0) return vet;
+  std::vector<int> vetRepetidos;
+
+  for(auto i = 0; i < vet.size(); i++){
+      for(auto j = i + 1; j < vet.size(); j++){
+        if(vet[i] == vet[j]){
+          vetRepetidos.push_back(vet[i]);
+          break;
+        }
+      }
+   }
+  return sort(vetRepetidos);
+}
 
 int main(){
-   auto strToVet = [](auto s) { return s.substr(1, s.size() - 2) | fn::SPLIT(',') | fn::MAP(FNT(x, (int)+x)); };
+   auto strToVet = [](auto s) { return fn::map(fn::split(s.substr(1, s.size() - 2), ','), FNT(x, (int)+x)); };
 
     while(true) {
         std::string line = fn::input();
         auto args = fn::split(line, ' ');
         fn::write('$' + line);
 
-      if (args[0] == "get_men" ) {    
-      fn::write(get_men(fn::TOSTR(args[1])));        
-      } else if(args[0] == "end" ) { 
-        break;                                    
+      if (args[0] == "get_men" ) {         
+      fn::write(get_men(strToVet(args[1])));  
+      }  else if(args[0] == "get_calm_women"      ) {   
+      fn::write(get_calm_women(strToVet(args[1]))); 
+      } else if(args[0] == "sort"  ) {
+      fn::write(sort(strToVet(args[1])));           
+      } else if(args[0] == "sort_stress" ) { 
+      fn::write(sort_stress(strToVet(args[1])));    
+      } else if(args[0] == "reverse"  ) {     
+      fn::write(reverse(strToVet(args[1])));        
+      } else if(args[0] == "unique"         ) { 
+      fn::write(unique(strToVet(args[1])));         
+      } else if(args[0] == "repeated"       ) {              
+      fn::write(repeated(strToVet(args[1])));       
+      } else if(args[0] == "end"  ) { 
+      break; 
       }
-
-    }
+      
+     }
 }
